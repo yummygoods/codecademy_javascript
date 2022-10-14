62e664b559e153ed2b84f60cb02f0cad9dc55b26
yummygoods <melissa@yummygoods.com>
2022-10-14 14:31:06 -0400

    add whale talk

--- secret_message.js
+++ secret_message.js
@@ -1,1 +1,19 @@
-https://www.codecademy.com/courses/introduction-to-javascript/projects/secret-message
+/*https://www.codecademy.com/courses/introduction-to-javascript/projects/secret-message */
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+console.log(secretMessage);
+console.log(secretMessage.length);
+secretMessage.push('to', 'Program');
+console.log(secretMessage);
+console.log(secretMessage.indexOf('easily'));
+secretMessage.splice(7, 1, 'right');
+console.log(secretMessage);
+secretMessage.shift()
+console.log(secretMessage);
+secretMessage.unshift('Programming')
+console.log(secretMessage);
+console.log(secretMessage.indexOf('get'));
+secretMessage.splice(6, 5, 'know');
+console.log(secretMessage);
+console.log(secretMessage.join(''));
---
+++ whale_talk.js
@@ -0,0 +1,13 @@
+let input = 'What in the actual fuck?';
+ let vowels = [ 'a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+for ( i = 0; i < input.length; i++ ) {
+  console.log('i is ' + i);
+  for (i = 0; i < vowels.length; i++){
+    console.log('i is ' + i);
+  }
+}
+
+/*
+console.log(input.length);
+*/
