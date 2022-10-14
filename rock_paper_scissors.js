61f3b5111788bd615a053c8537ef01670d259438
yummygoods <melissa@yummygoods.com>
2022-10-14 12:14:46 -0400

    complete rock paper scissors

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,15 +1,81 @@
-/*Rock paper scissors is a classic two player game. Each player chooses either rock, paper, or scissors. The items are compared, and whichever player chooses the more powerful item wins.
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+if (userInput === 'rock' || userInput === 'paper' ||  userInput === 'scissors') {
+    return userInput
+}
+else {
+    console.log("Bummer, you didn't enter one of the valid choices.")
+}
+};
 
-The possible outcomes are:
+/*
+console.log(getUserChoice('Scissors')); 
+*/
 
-Rock destroys scissors.
-Scissors cut paper.
-Paper covers rock.
-If there’s a tie, then the game ends in a draw.
-Our code will break the game into four parts:
 
-Get the user’s choice.
-Get the computer’s choice.
-Compare the two choices and determine a winner.
-Start the program and display the results.
-.*/
+const getComputerChoice = () => {
+let computerChoice = Math.floor(Math.random() * 3);
+    
+    if (computerChoice === 0) {
+        return 'rock';
+        }
+        else if (computerChoice === 1) {
+            return 'paper';
+        }
+        else if (computerChoice === 2) {
+            return 'scissors';
+        }
+}
+/*
+console.log(getComputerChoice());
+*/
+
+
+let determineWinner = (userChoice, computerChoice)  => {
+
+ if (userChoice === computerChoice) 
+                {
+  return 'The game is a tie!';
+                 }
+
+ if (userChoice === 'rock') 
+              {
+      if (computerChoice === 'paper') {
+           return 'The computer won!';
+              } 
+      
+    else {
+        return 'You won!';
+           }
+       }
+
+ if (userChoice === 'paper') {
+  if (computerChoice === 'scissors') {
+    return 'The computer won!';
+  }
+  else {
+    return 'You won!';
+  }
+}
+if(userChoice === 'scissors') {
+  if (computerChoice === 'rock') {
+    return 'The computer won!';
+  }
+  else {
+    return 'You won!';
+  }
+}
+}
+
+/*
+console.log(determineWinner('scissors' , 'paper'));
+*/
+
+const playGame = () => {
+    let userChoice = getUserChoice('paper');
+    let computerChoice = getComputerChoice();
+    console.log('You threw ' + userChoice);
+    console.log('The computer threw ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+    }
+    playGame()
